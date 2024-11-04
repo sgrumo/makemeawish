f35154302e042ae6ed95268cbb33b3aedb2b50e6
Samuele Medici <samuele.medici@quinck.io>
2024-11-04 12:43:26 +0100

    feat(auth): :sparkles: add signup call

--- src/app/(unprotected)/signup/page.tsx
+++ src/app/(unprotected)/signup/page.tsx
@@ -1,22 +1,38 @@
 'use client'
 
+import Input from '@/app/components/forms/input'
+import { signUp } from '@/lib/firebase/auth'
 import { AuthCredentials, AuthCredentialsSchema } from '@/lib/schemas/auth'
+import { ResultType } from '@/lib/utils/algebraic'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
+import { match } from 'ts-pattern'
 
 const SignupPage = () => {
-    const { handleSubmit } = useForm<AuthCredentials>({
+    const { handleSubmit, register } = useForm<AuthCredentials>({
         resolver: zodResolver(AuthCredentialsSchema),
         mode: 'onChange',
     })
 
-    const signupSubmit = (credentials: AuthCredentials) => {
-        console.log(credentials)
+    const signupSubmit = async (credentials: AuthCredentials) => {
+        const res = await signUp(credentials)
+        match(res)
+            .with({ resultType: ResultType.Ok }, () => {})
+            .with({ resultType: ResultType.Error }, () => {})
+            .exhaustive()
     }
 
     return (
         <>
-            <form onSubmit={handleSubmit(signupSubmit)}></form>
+            <form onSubmit={handleSubmit(signupSubmit)}>
+                <Input type="email" register={register} fieldName="email" />
+                <Input
+                    type="password"
+                    register={register}
+                    fieldName="password"
+                />
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
